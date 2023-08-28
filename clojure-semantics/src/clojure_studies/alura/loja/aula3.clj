7a349053b04bc4fe2074e65b99cf63915c8d96be
Felipe Pelichero <pelichero_13@hotmail.com>
2023-08-28 18:36:50 -0300

    isolating

--- clojure-semantics/src/clojure_studies/alura/loja/aula3.clj
+++ clojure-semantics/src/clojure_studies/alura/loja/aula3.clj
@@ -1,28 +1,6 @@
 (ns clojure-studies.alura.loja.aula3
-  (:require [clojure-studies.alura.loja.db :as db]))
+  (:require [clojure-studies.alura.loja.db :as db]
+            [clojure-studies.alura.loja.logic :as logic]))
 
-(defn total-do-item
-  [[_ detalhes]]
-  (* (get detalhes :quantidade 0) (get detalhes :preco-unitario 0)))
-
-(defn total-do-pedido
-  [pedido]
-  (reduce + (map total-do-item pedido)))
-
-(defn total-dos-pedidos
-  [pedidos]
-  (->> pedidos
-    (map :itens)
-    (map total-do-pedido)
-    (reduce +)))
-
-(defn quantidade-de-pedidos-e-gasto-total-por-usuario
-  [[usuario pedidos]]
-  {:usuario-id usuario
-   :total-de-pedidos (count pedidos)
-   :preco-total (total-dos-pedidos pedidos)})
-
-(->> (db/todos-pedidos)
-     (group-by :usuario)
-     (map quantidade-de-pedidos-e-gasto-total-por-usuario)
-     println)
+(let [resumo (logic/resumo-por-usuario (db/todos-pedidos))]
+  (println resumo))
