95fcd987efc7e9891767ec113494101a2162ecff
Felipe Pelichero <pelichero_13@hotmail.com>
2023-08-28 18:24:30 -0300

    reduce complex itens

--- clojure-semantics/src/clojure_studies/alura/loja/aula3.clj
+++ clojure-semantics/src/clojure_studies/alura/loja/aula3.clj
@@ -1,14 +1,28 @@
 (ns clojure-studies.alura.loja.aula3
   (:require [clojure-studies.alura.loja.db :as db]))
 
+(defn total-do-item
+  [[_ detalhes]]
+  (* (get detalhes :quantidade 0) (get detalhes :preco-unitario 0)))
 
-(defn conta-total-por-usuario
+(defn total-do-pedido
+  [pedido]
+  (reduce + (map total-do-item pedido)))
+
+(defn total-dos-pedidos
+  [pedidos]
+  (->> pedidos
+    (map :itens)
+    (map total-do-pedido)
+    (reduce +)))
+
+(defn quantidade-de-pedidos-e-gasto-total-por-usuario
   [[usuario pedidos]]
-  {:usuario-id       usuario
-   :total-de-pedidos (count pedidos)}
-  )
+  {:usuario-id usuario
+   :total-de-pedidos (count pedidos)
+   :preco-total (total-dos-pedidos pedidos)})
 
 (->> (db/todos-pedidos)
      (group-by :usuario)
-     (map conta-total-por-usuario)
+     (map quantidade-de-pedidos-e-gasto-total-por-usuario)
      println)
