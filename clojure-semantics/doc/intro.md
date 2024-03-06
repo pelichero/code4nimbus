a0919d54634d2a4b9b34c2882e19173635eb1e20
Felipe Pelichero <pelichero_13@hotmail.com>
2024-03-06 09:04:43 -0300

    fixing

--- clojure-semantics/doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to clojure-semantics
-
-TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
--- clojure-semantics/src/clojure_studies/alura/loja/aula3.clj
+++
@@ -1,6 +0,0 @@
-(ns clojure-studies.alura.loja.aula3
-  (:require [clojure-studies.alura.loja.db :as db]
-            [clojure-studies.alura.loja.logic :as logic]))
-
-(let [resumo (logic/resumo-por-usuario (db/todos-pedidos))]
-  (println resumo))
