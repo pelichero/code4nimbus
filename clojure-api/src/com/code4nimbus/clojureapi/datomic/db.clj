acb241f2559a85bcf6b5022edff8352f32eeb5c1
Felipe Pelichero <pelichero_13@hotmail.com>
2024-04-04 08:37:44 -0300

    add mem database test

--- clojure-api/src/com/code4nimbus/clojureapi/datomic/db.clj
+++ clojure-api/src/com/code4nimbus/clojureapi/datomic/db.clj
@@ -3,22 +3,25 @@
             [datomic.api :as d]
             [com.code4nimbus.clojureapi.model.product :as model.product]))
 
-(def uri (str (System/getenv "DATOMIC_URL")))
+(def uri (if (nil? (System/getenv "DATOMIC_URL"))
+           "datomic:mem://product"
+           (str (System/getenv "DATOMIC_URL"))))
 
 (defn get-conn
   []
+  (log/info (str "Connecting to database: " uri))
   (d/connect uri))
 
 (defn create-database
   []
   (d/create-database uri)
-  (log/info "Database created"))
+  (log/info (str "Database created " uri)))
 
 (defn drop-database
   []
   (try
     (d/delete-database uri)
-    (log/info "Database dropped")
+    (log/info (str "Database dropped " uri))
     (catch Exception ex
       (log/error "Database does not exist; ex: " ex))))
 
@@ -26,7 +29,7 @@
   []
   (try
     (d/transact (d/connect uri) model.product/product-schema)
-    (log/info "Schema created")
+    (log/info (str "Schema created " uri))
     (catch Exception error
       (log/error "Error creating schema, ex: " error))))
 
