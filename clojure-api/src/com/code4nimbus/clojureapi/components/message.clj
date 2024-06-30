70d3c8a3e7ff6c6157868aef171029e483a119ad
Felipe Pelichero <pelichero_13@hotmail.com>
2024-06-30 11:56:57 -0300

    componentizing kafka

--- clojure-api/src/com/code4nimbus/clojureapi/components/message.clj
+++ clojure-api/src/com/code4nimbus/clojureapi/components/message.clj
@@ -1,8 +1,31 @@
 (ns com.code4nimbus.clojureapi.components.message
   (:require [clojure.tools.logging :as log]
-            [com.code4nimbus.clojureapi.diplomat.consumer :as diplomat.consumer]))
+            [com.stuartsierra.component :as component])
+  (:import (java.time Duration)
+           org.apache.kafka.clients.consumer.KafkaConsumer
+           (org.apache.kafka.common.serialization StringDeserializer)))
 
-(defn ^:private configure-kafka
-  [conn]
-  (log/info "Starting Kafka listeners...")
-  (diplomat.consumer/product-listener conn))
+(defn build-consumer
+  [bootstrap-server]
+  (let [consumer-props
+        {"bootstrap.servers",  bootstrap-server
+         "group.id",           "example"
+         "key.deserializer",   StringDeserializer
+         "value.deserializer", StringDeserializer
+         "auto.offset.reset",  "earliest"
+         "enable.auto.commit", "true"}]
+    (KafkaConsumer. consumer-props)))
+
+(defrecord Kafka [db topic bootstrap-server handler] component/Lifecycle
+  (start [this]
+    (log/info (str "Starting Kafka listeners on server " bootstrap-server " for topic: " topic))
+    (let [consumer (build-consumer bootstrap-server)]
+      (.subscribe consumer [topic])
+      (while true
+        (doseq [record (.poll consumer (Duration/ofMillis 100))]
+          (handler record db))
+        (.commitAsync consumer))
+      (assoc this :consumer consumer)))
+  (stop [this]
+    (log/info "Stopping Kafka listeners...")
+    this))
